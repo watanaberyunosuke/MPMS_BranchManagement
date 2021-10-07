b41fd587cc57b1be0b587717ffb8c8d975b2ba4f
watanaberyunosuke <harrybwatson@protonmail.com>
2021-10-07 17:09:34 +1100

    Stage progress of Branch.class, finished search functions
    Signed-off-by: watanaberyunosuke <harrybwatson@protonmail.com>

--- src/com/monash/MPMS/Branch.java
+++ src/com/monash/MPMS/Branch.java
@@ -2,16 +2,15 @@ package com.monash.MPMS;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
 /**
  * Start of class
  */
 
-public class Branch{
-    private final int branchId;
-    private String branchName;
-    private String branchPostcode;
+public class Branch {
+    String branchId;
+    String branchName;
+    String branchPostcode;
     private final List<String> branchNameList = new ArrayList<>();
     private final List<String> branchPostcodeList = new ArrayList<>();
 
@@ -20,20 +19,20 @@ public class Branch{
      */
 
     public Branch() {
-        branchId = 0;
+        branchId = "";
         branchName = "";
         branchPostcode = "";
-
     }
 
     /**
      * Parameterised constructor
-     * @param branchId: Int of id of branch
-     * @param branchName: String() name of branch
+     *
+     * @param branchId:       Int of id of branch
+     * @param branchName:     String() name of branch
      * @param branchPostcode: String() the postcode of branch
      */
 
-    public Branch(int branchId, String branchName,String branchPostcode){
+    public Branch(String branchId, String branchName, String branchPostcode) {
         this.branchId = branchId;
         this.branchName = branchName;
         this.branchPostcode = branchPostcode;
@@ -44,112 +43,56 @@ public class Branch{
      */
     public void loadBranch() {
 
-        // load file
+        // load file with error handling
         try {
             File branchFileObj = new File("src/com/monash/MPMS/Branch.txt");
             Scanner fileReader = new Scanner(branchFileObj);
             while (fileReader.hasNextLine()) {
                 String branchInformation = fileReader.nextLine();
                 String[] branchInfoArray = branchInformation.split(",", 3);
-                if(branchInfoArray.length >= 2){
+                if (branchInfoArray.length >= 2) {
                     branchNameList.add(branchInfoArray[1]);
                     branchPostcodeList.add(branchInfoArray[2].trim());
                 }
 
             }
             fileReader.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("An error has occurred...");
             e.printStackTrace();
         }
     }
 
-    /**
-     * Mutator
-     */
+        /**
+         * Mutator
+         */
 
-    public void setBranchName(){
-        this.branchName = branchName;
-    }
-
-    public void setBranchPostcode(){
-        this.branchPostcode = branchPostcode;
-    }
-
-    /**
-     * Accessor
-     */
-
-    public String getBranchName(){
-        return this.branchName;
-    }
-
-    public Integer getBranchId(){
-        return this.branchId;
-    }
-
-    /**
-     * Get User Input of Postcode
-     */
-
-    public void searchBranchPC() {
-        // init postcodeEnter variable
-        String postcodeEnter;
-        // Take user input
-        System.out.println("Please enter the postcode of branch you are looking for...\n");
-        Scanner sc = new Scanner(System.in);
-        postcodeEnter = sc.nextLine();
-
-        // Check if input matches pattern of postcodeEnter
-        if(postcodeEnter.trim().matches("[0-9]+") && postcodeEnter.length() == 4) {
-            System.out.println("The postcode is valid...Now loading database...");
-            loadBranch();
-        }
-        else if(postcodeEnter.length() == 0){
-            System.out.println("The postcode can not be empty...");
-            searchBranchPC();
-        }
-        else if(postcodeEnter.length() < 4){
-            System.out.println("The postcode you enter is too short...");
-            searchBranchPC();
-        }
-        else{
-            System.out.println("The postcode you have entered is not correct! Please check again.");
-            searchBranchPC();
+        public void setBranchName () {
+            this.branchName = branchName;
         }
 
-
-        // Find by postcode
-        // Return clinic name by index
-        // TODO: Change iteration from adding to iterating to check
-        // Count occurrence of postcode entered
-        int occurrence;
-        occurrence = Collections.frequency(branchPostcodeList, postcodeEnter);
-        if(occurrence > 0){
-            // Find index of all matching branches
-            // https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html
-            int[] postcodeSearchIndex = IntStream.range(0, branchPostcodeList.size())
-                    .filter(i -> postcodeEnter.equals(branchPostcodeList.get(i)))
-                    .toArray();
-            // Return result
-            List<String> postcodeSearchResults = new ArrayList<>();
-            for (int searchIndex : postcodeSearchIndex) {
-                String output = branchNameList.get(searchIndex);
-                postcodeSearchResults.add(output.trim());
-            }
-            System.out.println("The following branches are in this area: ");
-            for(int i = 0; i < postcodeSearchResults.size();i++){
-                System.out.println((i+1) + ". " + postcodeSearchResults.get(i));
-            }
-
+        public void setBranchPostcode () {
+            this.branchPostcode = branchPostcode;
         }
-        else {
-            System.out.println("We can not find the branch that you are looking for...");
-            searchBranchPC();
+
+        /**
+         * Accessor and Mutator
+         */
+
+        public String getBranchId() {
+            return branchId;
         }
-    }
 
+        public void setBranchId(String branchId) {
+            this.branchId = branchId;
+        }
 
+        public String getBranchPostcode() {
+            return branchPostcode;
+        }
+
+        public void setBranchPostcode(String branchPostcode) {
+            this.branchPostcode = branchPostcode;
+        }
 }
 
--- src/com/monash/MPMS/BranchManagementSystem.java
+++ src/com/monash/MPMS/BranchManagementSystem.java
@@ -1,8 +1,16 @@
 package com.monash.MPMS;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class BranchManagementSystem {
+    // TODO: Consider changing data structure, use hashMap or hashTable
+    //TODO 2: Change visualbility and lifetime of var: postcodeSearch
+    private final List<Branch> branchList = new ArrayList<Branch>();
+    private final List<GP> gpList = new ArrayList<GP>();
+    private String[] postcodeSearch;
+
+
+
     public BranchManagementSystem() {
 
     }
@@ -24,7 +32,6 @@ public class BranchManagementSystem {
         switch (input) {
             case 1: {
                 Branch branchObj = new Branch();
-                branchObj.searchBranchPC();
                 break;
             }
             case 2:{
@@ -42,6 +49,79 @@ public class BranchManagementSystem {
         }
     }
 
+    /**
+     * Get User Input of Postcode
+     * @param branchObj
+     */
+
+    public void searchBranchPC(Branch branchObj) {
+        // init postcodeEnter variable
+        String postcodeEnter;
+        // Take user input
+        System.out.println("Please enter the postcode of branch you are looking for...\n");
+        Scanner sc = new Scanner(System.in);
+        postcodeEnter = sc.nextLine();
+
+        // Check if input matches pattern of postcodeEnter
+        if(postcodeEnter.trim().matches("[0-9]+") && postcodeEnter.length() == 4) {
+            System.out.println("The postcode is valid...");
+            Branch branch = new Branch();
+        }
+        else if(postcodeEnter.length() == 0){
+            System.out.println("The postcode can not be empty...");
+            searchBranchPC(branchObj);
+        }
+        else if(postcodeEnter.length() < 4){
+            System.out.println("The postcode you enter is too short...");
+            searchBranchPC(branchObj);
+        }
+        else{
+            System.out.println("The postcode you have entered is not correct! Please check again.");
+            searchBranchPC(branchObj);
+        }
+
+
+        // Find by postcode
+        // Return clinic name by index
+        // Count occurrence of postcode entered
+
+        int occurrence;
+        occurrence = Collections.frequency((Collection<?>) branchObj, postcodeEnter);
+        if(occurrence > 0){
+            // Find index of all matching branches using lambda function stream
+            // https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html
+            String[] postcodeSearch = (String[]) branchList.stream()
+                    .filter(searchID -> (Branch.getBranchPostcode()).equals(searchID) || Branch.getBranchPostcode().equals(postcodeEnter))
+                    .toArray();
+            // Return result
+            System.out.println("The following branches are in this area: ");
+            for(int i = 0; i < postcodeSearch.length;i++){
+                System.out.println((i+1) + ". " + postcodeSearch[i]);
+                this.postcodeSearch = postcodeSearch;
+            }
+
+        }
+        else {
+            System.out.println("We can not find the branch that you are looking for...");
+            searchBranchPC(branchObj);
+        }
+        
+        // Select Branch to see detail
+        System.out.println("Please select a branch to see it's detail");
+        int userChoice = sc.nextInt();
+        // Error handling
+        if(userChoice > postcodeSearch.length){
+            System.out.println("There are not that many item in the result....");
+        }
+        else if(userChoice <= 0){
+            System.out.println("This number is too small...");
+        }
+        else{
+            // target
+            return;
+        }
+
+    }
 
 
 }
