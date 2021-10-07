06f6a54ab52db6d1be800183fa3e20aa0414fa93
watanaberyunosuke <harrybwatson@protonmail.com>
2021-10-07 21:33:41 +1100

    Change .idea file
    Signed-off-by: watanaberyunosuke <harrybwatson@protonmail.com>

--- src/com/monash/MPMS/Login.java
+++ src/com/monash/MPMS/Login.java
@@ -1,5 +1,7 @@
 package com.monash.MPMS;
 
+import java.util.Scanner;
+
 public class Login {
     /**
      * Constructor
@@ -17,6 +19,33 @@ public class Login {
         System.out.println("Please Select Your User Group");
         System.out.println("1. Patient");
         System.out.println("2. Admin");
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+        switch (userInput){
+            case 1:
+                patientHome();
+            case 2:
+                adminHome();
+            default:
+                System.out.println("This is not an valid option");
+                selectUserClass();
+        }
 
+    }
+
+    /**
+     * Patient Home Page
+     */
+
+    public void patientHome() {
+        System.out.println("Welcome to patient homepage");
+    }
+
+    /**
+     * Admin Home Page
+     */
+
+    public void adminHome() {
+        System.out.println("Welcome to admin homepage");
     }
 }
