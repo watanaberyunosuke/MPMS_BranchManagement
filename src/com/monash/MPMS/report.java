2843c479a9915d3fb4b235ceb7dc509237c759c1
albert <shua0059@student.monash.edu>
2021-10-14 15:23:36 +1100

    generate index

--- src/com/monash/MPMS/report.java
+++ src/com/monash/MPMS/report.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 
 public class report {
     String[] index= null;
+    boolean flag = true;
 
 
     public int[][] getReasonTime() {
         try{
+            String indexString = "";
             fr= new FileReader("trees.txt");
             BufferedReader bufferedReader = new BufferedReader(fr);
-            while ()
+            indexString = bufferedReader.readLine();
+            index = indexString.split(",");
         }
         catch (IOException e){
             System.out.println("Input Error!");
