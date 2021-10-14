77b2709155653697aad25bc37dd2650eecf19bd7
jfal0001 <jfal0001@student.monash.edu>
2021-10-14 15:55:07 +1100

    Replace Login.java

--- src/com/monash/MPMS/Login.java
+++ src/com/monash/MPMS/Login.java
@@ -2,49 +2,91 @@ package com.monash.MPMS;
 import java.util.Scanner;
 
 public class Login {
-    static int userClass;
+    private static int userClass;
+    private static String password;
+    private static String email;
 
     public Login() {
         userClass = 0;
+        password = "";
+        email = "";
     }
 
-    public static void patientLogin()
+    static void loginScreen()
     {
-        if (userClass == 1) {
-
-
-        }
-
-
-    }
-
-    public static void main(String[] args)
-    {
-        int userClass;
         Scanner newSc = new Scanner(System.in);
-        System.out.println("Please enter your email.");
+        System.out.println("************************");
+        System.out.println("    Login Screen");
+        System.out.println("************************");
+        System.out.println("Enter your email address:");
         String email;
         email = newSc.nextLine();
-        System.out.println("Email is: " + email);
-        System.out.println("Please enter your password");
+        System.out.println("Enter your email address: " + email);
+        System.out.println("Enter your password:");
         String password;
         password = newSc.nextLine();
-        System.out.println("Password: " + password);
+        System.out.println("Enter your password: " + password);
         if (email.equals("patient@monash.edu") && password.equals("Monash1234"))
         {
-            System.out.println("Welcome patient.");
             userClass = 1;
         }
         else if (email.equals("admin@monash.edu") && password.equals("Monash1234"))
         {
-            System.out.println("Welcome admin.");
             userClass = 2;
         }
         else
         {
             System.out.println("Sorry, email or password is incorrect. Please try again.");
         }
-        patientLogin();
+        homePage();
+    }
+
+    static void homePage()
+    {
+        if (userClass == 1)
+        {
+            patientLogin();
+        }
+        else if (userClass == 2)
+        {
+            adminLogin();
+        }
+        else
+        {
+            loginScreen();
+        }
+    }
+
+    static void adminLogin()
+    {
+        System.out.println("************************");
+        System.out.println("    Admin Home Page");
+        System.out.println("************************");
+        System.out.println("Please choose an option:");
+        System.out.println("1. Clinic Management");
+        System.out.println("2. GP Management");
+        System.out.println("3. Status Dashboard");
+        System.out.println("4. Log out");
+
+        }
+
+    static void patientLogin()
+    {
+        System.out.println("************************");
+        System.out.println("    Patient Home Page");
+        System.out.println("************************");
+        System.out.println("1. Cancel Appointment.");
+        System.out.println("2. Select favourite branch and GP.");
+        System.out.println("3. Check details of a branch.");
+        System.out.println("4. Book an Appointment.");
+        System.out.println("5. Check in to Appointment.");
+        System.out.println("Esc. back to Branch Locator.");
+    }
+
+
+    public static void main(String[] args)
+    {
+        loginScreen();
     }
 }
 
