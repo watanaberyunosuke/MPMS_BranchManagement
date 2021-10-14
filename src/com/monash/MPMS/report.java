4f738a41e9360295a213f565e5b72041e6583d0b
albert <shua0059@student.monash.edu>
2021-10-14 15:05:16 +1100

    filereader

--- src/com/monash/MPMS/report.java
+++ src/com/monash/MPMS/report.java
@@ -1,4 +1,20 @@
 package com.monash.MPMS;
+import java.io.*;
+import java.util.Arrays;
 
 public class report {
+    String[] index= null;
+
+
+    public int[][] getReasonTime() {
+        try{
+            fr= new FileReader("trees.txt");
+            BufferedReader bufferedReader = new BufferedReader(fr);
+            while ()
+        }
+        catch (IOException e){
+            System.out.println("Input Error!");
+        }
+    }
+
 }
