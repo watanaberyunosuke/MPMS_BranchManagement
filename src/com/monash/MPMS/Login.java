39361ddd3e1279d0ae1893a67d7c141d985aca19
jfal0001 <jfal0001@student.monash.edu>
2021-10-15 23:19:42 +1100

    Replace Login.java

--- src/com/monash/MPMS/Login.java
+++ src/com/monash/MPMS/Login.java
@@ -7,6 +7,9 @@ public class Login {
     private static String email;
 
     public Login() {
+        /**
+         * Constructor for Login class
+         */
         userClass = 0;
         password = "";
         email = "";
@@ -14,6 +17,9 @@ public class Login {
 
     static void startScreen()
     {
+        /**
+         * Display the initial login screen
+         */
         System.out.println("************************");
         System.out.println("      Login Screen      ");
         System.out.println("************************");
@@ -23,11 +29,11 @@ public class Login {
         option = 0;
         Scanner newSc = new Scanner(System.in);
         option = newSc.nextInt();
-        if (option == 1)
+        if (option == 1) //user wants to login
         {
             loginScreen();
         }
-        else if (option == 2)
+        else if (option == 2) //user wants to exit the system
         {
             System.out.println("Exiting System.");
         }
@@ -39,6 +45,9 @@ public class Login {
 
     static void loginScreen()
     {
+        /**
+         * Display the screen that the user can log in from.
+         */
         Scanner newSc = new Scanner(System.in);
         System.out.println("************************");
         System.out.println("    Login Screen");
@@ -51,13 +60,13 @@ public class Login {
         String password;
         password = newSc.nextLine();
         System.out.println("Enter your password: " + password);
-        if (email.equals("patient@monash.edu") && password.equals("Monash1234"))
+        if (email.equals("patient@monash.edu") && password.equals("Monash1234")) //check that email and password match what's in the system
         {
-            userClass = 1;
+            userClass = 1; //if user is considered a patient
         }
         else if (email.equals("admin@monash.edu") && password.equals("Monash1234"))
         {
-            userClass = 2;
+            userClass = 2; //if user is considered an admin
         }
         else
         {
@@ -67,7 +76,11 @@ public class Login {
     }
 
     static void homePage()
+
     {
+        /**
+         * method that decides whether to take the user to the patient home page or admin home page
+         */
         if (userClass == 1)
         {
             patientLogin();
@@ -84,6 +97,9 @@ public class Login {
 
     static void adminLogin()
     {
+        /**
+         * Method that displays the admin home page
+         */
         System.out.println("************************");
         System.out.println("    Admin Home Page");
         System.out.println("************************");
@@ -92,19 +108,49 @@ public class Login {
         System.out.println("2. GP Management");
         System.out.println("3. Status Dashboard");
         System.out.println("4. Log out");
+        int option;
+        option = 0;
+        Scanner newSc = new Scanner(System.in);
+        option = newSc.nextInt();
+        if (option == 1)
+        {
+            System.out.println("Clinic Management");
+        }
+        else if (option == 2)
+        {
+            System.out.println("GP Management");
+        }
+        else if (option == 3)
+        {
+            System.out.println("Status Dashboard");
+            // Status dashboard
+        }
+        else if (option == 4)
+        {
+            System.out.println("Returning to login screen");
+            startScreen();
+        }
+        else
+        {
+            System.out.println("Please select an option that is listed.");
+            adminLogin();
+        }
 
         }
 
     static void patientLogin()
     {
+        /**
+         * Method that displays the patient home page.
+         */
         System.out.println("*************************");
         System.out.println("    Patient Home Page");
         System.out.println("*************************");
-        System.out.println("1. Cancel Appointment.");
-        System.out.println("2. Select favourite branch and GP.");
+        System.out.println("1. Cancel Appointment (not completed).");
+        System.out.println("2. Select favourite branch and GP (not completed).");
         System.out.println("3. Check details of a branch.");
         System.out.println("4. Book an Appointment.");
-        System.out.println("5. Check in to Appointment.");
+        System.out.println("5. Check in to Appointment. (not completed)");
         System.out.println("6. Return to Login screen.");
         int option;
         option = 0;
