2100a3df6b372930657afa968b34a2ed89d52b7f
jfal0001 <jfal0001@student.monash.edu>
2021-10-20 22:52:11 +1100

    Replace Branch.java

--- src/com/monash/MPMS/Branch.java
+++ src/com/monash/MPMS/Branch.java
@@ -1,72 +1,120 @@
 package com.monash.MPMS;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+import java.io.FileWriter;
 
-/**
- * Start of class
- */
 
 public class Branch {
-    String branchId;
-    String branchName;
-    String branchPostcode;
-    private final List<String> branchNameList = new ArrayList<>();
-    private final List<String> branchPostcodeList = new ArrayList<>();
-
-    /**
-     * Default Constructor
-     */
+    private static List<String> branchID = new ArrayList<>();
+    private static List<String> branchName = new ArrayList<>();
+    private static List<String> branchPC = new ArrayList<>();
+    private static List<String> branchAddress = new ArrayList<>();
+    private static List<String> branchOpeningHours = new ArrayList<>();
+    private static List<String> branchPhone = new ArrayList<>();
+    private static String selectedBranch;
 
     public Branch() {
-        branchId = "";
-        branchName = "";
-        branchPostcode = "";
+        selectedBranch = "";
+
     }
 
-    /**
-     * Parameterised constructor
-     *
-     * @param branchId:       Int of id of branch
-     * @param branchName:     String() name of branch
-     * @param branchPostcode: String() the postcode of branch
-     */
-
-    public Branch(String branchId, String branchName, String branchPostcode) {
-        this.branchId = branchId;
-        this.branchName = branchName;
-        this.branchPostcode = branchPostcode;
+    static void loadBranch()
+    {
+        /**
+         * Method to load the Branch text file into separate array lists
+         */
+        // load file with error handling
+        try
+        {
+            File branchFileObj = new File("src/com/monash/MPMS/Branch.txt");
+            Scanner fileReader = new Scanner(branchFileObj);
+            while (fileReader.hasNextLine())
+            {
+                String branchInformation = fileReader.nextLine();
+                String[] branchInfoArray = branchInformation.split(",", 6);
+                if (branchInfoArray.length >= 5)
+                {
+                    branchID.add(branchInfoArray[0].trim());
+                    branchName.add(branchInfoArray[1].trim());
+                    branchPC.add(branchInfoArray[2].trim());
+                    branchAddress.add(branchInfoArray[3].trim());
+                    branchOpeningHours.add(branchInfoArray[4].trim());
+                    branchPhone.add(branchInfoArray[5].trim());
+                }
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("An error has occurred...");
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * Mutator
-     */
-
-    public void setBranchName() {
-        this.branchName = branchName;
+    static void clinicSelection() {
+        loadBranch();
+        System.out.println("****************************");
+        System.out.println("     Check Clinic Details   ");
+        System.out.println("****************************");
+        System.out.println("Please select a Clinic:");
+        int userChoice = 1;
+        for (String element : branchName) {
+            System.out.println(userChoice + ". " + element);
+            userChoice = userChoice + 1;
+        }
+        System.out.println(userChoice + ". To return to Patient Home Page.");
+        int option;
+        Scanner newSc = new Scanner(System.in);
+        option = newSc.nextInt();
+        if (option > userChoice) {
+            System.out.println("Please select a number that's listed.");
+            clinicSelection();
+        } else if (option == userChoice) {
+            System.out.println("Returning to patient home page.");
+        } else {
+            option = option - 1;
+            selectedBranch = branchName.get(option);
+            System.out.println(selectedBranch);
+            // Show branch information
+            System.out.println("The information of this branch is as below");
+            System.out.println("Branch ID: " + branchID.get(option));
+            System.out.println("Branch Name: " + branchName.get(option));
+            System.out.println("Postcode: " + branchPC.get(option));
+            System.out.println("Address: " + branchAddress.get(option));
+            System.out.println("Open Time: " + branchOpeningHours.get(option));
+            System.out.println("Phone Number: " + branchPhone.get(option));
+            returnToPage();
+        }
     }
 
-    public void setBranchPostcode() {
-        this.branchPostcode = branchPostcode;
+    public static void returnToPage()
+    {
+        System.out.println("1. Search for another Branch.");
+        System.out.println("2. Return to Patient Home Page.");
+        int option;
+        Scanner newSc = new Scanner(System.in);
+        option = newSc.nextInt();
+        if (option == 2)
+        {
+            Login myObj = new Login();
+            Login.patientLogin();
+        }
+        else if (option == 1)
+        {
+            clinicSelection();
+        }
+        else
+        {
+            System.out.println("Please select a listed option.");
+            returnToPage();
+        }
     }
 
-    /**
-     * Accessor and Mutator
-     */
 
-    public String getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(String branchId) {
-        this.branchId = branchId;
-    }
-
-    public String getBranchPostcode() {
-        return branchPostcode;
-    }
-
-    public void setBranchPostcode(String branchPostcode) {
-        this.branchPostcode = branchPostcode;
+    public static void main(String[] args)
+    {
+        clinicSelection();
     }
 }
+
 
